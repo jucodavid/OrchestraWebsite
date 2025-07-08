f6068416abd95f2014b8066f52aef47afd8d9850
Mateo Bauvir <bauvir.mateo@gmail.com>
2025-07-08 22:47:34 +0200

    useless things

--- CSP_Planning/test/test.py
+++ CSP_Planning/test/test.py
@@ -1,9 +1,12 @@
-from scheduler_repetition import RepetitionScheduler
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from backend.scheduler_repetition import RepetitionScheduler
 
 sched = RepetitionScheduler(
-    "uploads/repartition_cetteannee.xlsx",
-    "uploads/disponibilites_exemple.xlsx",
-    4, 3, 2, 60
+    "../backend/uploads/repartition_cetteannee.xlsx",
+    "../backend/uploads/disponibilites_exemple.xlsx",
+    4, 3, 2, 60,3
 )
 sched.load_data()
 sched.build_model()
